d18effc5f8db4ca4536398db26d0d0d72ada15c0
Anton Österberg <anton.osterberg@gmail.com>
2017-08-13 19:09:26 +0200

    Got standard deviation now.

--- src/main/kotlin/Spread.kt
+++ src/main/kotlin/Spread.kt
@@ -1,19 +1,15 @@
-fun median(c: Collection<Number>): Double
-{
-	if(c.isEmpty())
-		return Double.NaN
-
-	return c.asSequence()
-			.median()
-
-}
+fun median(c: Collection<Number>): Double = c.asSequence().median()
+fun mean(c: Collection<Number>): Double = c.map { it.toDouble() }.average()
+fun range(c: Collection<Number>): Double = c.asSequence().range()
+fun variance(c: Collection<Number>): Double = c.asSequence().variance()
+fun standardDeviation(c: Collection<Number>): Double = Math.sqrt(variance(c))
 
 fun Sequence<Number>.median(): Double
 {
 	val size = this.asSequence().count()
 
-	val evenCount = size % 2 == 0
-	val elementsToDrop: Int = when(evenCount)
+	val evenSize = size % 2 == 0
+	val elementsToDrop: Int = when(evenSize)
 	{
 		true -> ((size/2.0) - 1).toInt()
 		false -> size/2
@@ -22,6 +18,29 @@ fun Sequence<Number>.median(): Double
 	return this.map { it.toDouble() }
 			.sorted()
 			.drop(elementsToDrop)
-			.take(if(evenCount) 2 else 1)
+			.take(if(evenSize) 2 else 1)
 			.average()
 }
+
+
+fun Sequence<Number>.range(): Double
+{
+	val min: Double = map { it.toDouble() }.min() ?: 0.0
+	val max: Double = map { it.toDouble() }.max() ?: 0.0
+	return max - min
+}
+
+fun Sequence<Number>.variance(): Double
+{
+	val doubleSequence: Sequence<Double> = map { it.toDouble() }
+	val mean = doubleSequence.average()
+
+	val sum = doubleSequence
+			.map { it - mean }
+			.map { Math.pow(it, 2.0) }
+			.sum()
+
+	return sum/count()
+}
+
+fun Sequence<Number>.standardDeviation(): Double = Math.sqrt(variance())
--- src/test/kotlin/StandardDeviationTest.kt
+++ src/test/kotlin/StandardDeviationTest.kt
@@ -1,10 +1,6 @@
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.assertEquals
 
-/**
- * @author Anton &Ouml;sterberg
- */
-
 class StandardDeviationTest
 {
 	@Test
@@ -14,9 +10,19 @@ class StandardDeviationTest
 	}
 
 	@Test
+	fun testRange()
+	{
+		val data: Collection<Number> = listOf(-5, 0, 5, 10, 15)
+		val range: Double = range(data)
+		assertEquals(20.0, range)
+	}
+
+	@Test
 	fun testMean()
 	{
-
+		val data: List<Int> = listOf(-1, 0, 1, 2, 3)
+		val mean: Double = mean(data)
+		assertEquals(mean, 1.0)
 	}
 
 	@Test
@@ -65,5 +71,36 @@ class StandardDeviationTest
 		val data: Collection<Number> = listOf(5, 1, 3, 4, 2)
 		val median: Double = median(data)
 		assertEquals(median, 3.0)
+	}
+
+	@Test
+	fun testVariance1()
+	{
+		val data: Collection<Number> = listOf(-10, 0, 10, 20, 30)
+		val variance = variance(data)
+		assertEquals(200.0, variance)
+	}
+
+	@Test
+	fun testVariance2()
+	{
+		val data: Collection<Number> = listOf(8, 9, 10, 11, 12)
+		val variance = variance(data)
+		assertEquals(2.0, variance)
+	}
+
+	@Test
+	fun testStandardDeviation1()
+	{
+		val data: Collection<Number> = listOf(8, 9, 10, 11, 12)
+		val sd = standardDeviation(data)
+		assertEquals(Math.sqrt(2.0), sd, 0.0000001)
+	}
+
+	@Test
+	fun testStandardDeviationSequence()
+	{
+		val sd = sequenceOf(8, 9, 10, 11, 12).standardDeviation()
+		assertEquals(Math.sqrt(2.0), sd, 0.0000001)
 	}
 }
